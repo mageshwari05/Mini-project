5476702c4c44363ee0d1ae6d2934b68a344ec8e2
MAGESGWARI M <mageshwarim9003998405@gmail.com>
2026-04-18 20:09:00 +0530

    Update TODO.py

--- TODO.py
+++ TODO.py
@@ -1,25 +1,60 @@
 print("TO DO LIST")
 
-print("1. Add Task")
-print("2. View Tasks")
-print("3. Exit")
+while True:
 
-choice = input("Enter choice: ")
+    print("\n1. Add Task")
+    print("2. View Tasks")
+    print("3. Delete Task")
+    print("4. Clear All Tasks")
+    print("5. Exit")
 
-if choice == "1":
-    task = input("Enter task: ")
-    file = open("TODO.txt","a")
-    file.write(task + "\n")
-    file.close()
-    print("Task Added")
+    choice = input("Enter choice: ")
 
-elif choice == "2":
-    file = open("TODO.txt","r")
-    print(file.read())
-    file.close()
+    # Add Task
+    if choice == "1":
+        task = input("Enter task: ")
+        file = open("todo.txt", "a")
+        file.write(task + "\n")
+        file.close()
+        print("Task Added")
 
-elif choice == "3":
-    print("Exit")
+    # View Tasks
+    elif choice == "2":
+        file = open("todo.txt", "r")
+        tasks = file.readlines()
+        file.close()
 
-else:
-    print("Invalid choice")
+        for i in range(len(tasks)):
+            print(i+1, tasks[i])
+
+    # Delete Task
+    elif choice == "3":
+        file = open("todo.txt", "r")
+        tasks = file.readlines()
+        file.close()
+
+        for i in range(len(tasks)):
+            print(i+1, tasks[i])
+
+        delete = int(input("Enter task number: "))
+        tasks.pop(delete-1)
+
+        file = open("todo.txt", "w")
+        file.writelines(tasks)
+        file.close()
+
+        print("Task Deleted")
+
+    # Clear All Tasks
+    elif choice == "4":
+        file = open("todo.txt", "w")
+        file.close()
+        print("All Tasks Cleared")
+
+    # Exit
+    elif choice == "5":
+        print("Exit")
+        break
+
+    else:
+        print("Invalid choice")
