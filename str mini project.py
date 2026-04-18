97b838155e68780616014c074bfe2f50b25e0d01
MAGESGWARI M <mageshwarim9003998405@gmail.com>
2026-04-18 20:10:09 +0530

    Update str mini project.py

--- str mini project.py
+++ str mini project.py
@@ -1,21 +1,28 @@
-string="learn python programming"
-print("\n1.word count \n2.vowels count \n3.exit")
-choice=int(input("Enter your choice:"))
-if choice==1:
-    print("words count")
-    count=1
-    for i in string:
-        if i==" ":
-            count=count+1
-    print("Count of words:",count)
-elif choice==2:
-    print("Vowels count")
-    count=0
-    for i in string:
-        if i in "aeiou":
-            count=count+1
-    print("Count of vowels",count)
-elif choice==3:
-    print("Thank you")
-else:
-    print("invalid number")
+string = "learn python programming"
+
+while True:
+    print("\n1.word count \n2.vowels count \n3.exit")
+    choice = int(input("Enter your choice: "))
+
+    if choice == 1:
+        print("words count")
+        count = 1
+        for i in string:
+            if i == " ":
+                count = count + 1
+        print("Count of words:", count)
+
+    elif choice == 2:
+        print("Vowels count")
+        count = 0
+        for i in string:
+            if i in "aeiou":
+                count = count + 1
+        print("Count of vowels", count)
+
+    elif choice == 3:
+        print("Thank you")
+        break
+
+    else:
+        print("invalid number")
