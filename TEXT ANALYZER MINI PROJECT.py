eb3072df5e757b15540ce4fdf9fce450fa3981ff
MAGESGWARI M <mageshwarim9003998405@gmail.com>
2026-05-01 17:02:03 +0530

    Update TEXT ANALYZER MINI PROJECT.py

--- TEXT ANALYZER MINI PROJECT.py
+++ TEXT ANALYZER MINI PROJECT.py
@@ -34,3 +34,38 @@ print("------")
 
 # Character count
 print("Character count:", len(data))
+
+#added more features
+print("more features")
+
+# Vowel count
+count = 0
+for i in data:
+    if i.lower() in "aeiou":
+        count += 1
+print("Vowel count:", count)
+
+print("------")
+
+# Count specific word
+word = input("Enter word to search: ")
+print("Count:", data.count(word))
+
+print("------")
+
+# Uppercase and Lowercase
+print("Uppercase:")
+print(data.upper())
+
+print("Lowercase:")
+print(data.lower())
+
+print("------")
+
+# Replace word
+old = input("Enter word to replace: ")
+new = input("Enter new word: ")
+new_data = data.replace(old, new)
+
+print("After replace:")
+print(new_data)
