422451fd190d5673373b92115bab219c7f6b9a8c
MAGESGWARI M <mageshwarim9003998405@gmail.com>
2026-05-02 19:17:00 +0530

    Update TEXT ANALYZER MINI PROJECT.py

--- TEXT ANALYZER MINI PROJECT.py
+++ TEXT ANALYZER MINI PROJECT.py
@@ -1,71 +1,62 @@
-print("TEXT ANALYZER")
-
-# Write to file
+print("TEXT ANALYZER PROJECT")
+# Write data to file
 file = open("text.txt","w")
 text = input("Enter text: ")
 file.write(text)
 file.close()
+while True:
+    print("\n1. Show Content")
+    print("2. Word Count")
+    print("3. Line Count")
+    print("4. Character Count")
+    print("5. Vowel Count")
+    print("6. Search Word")
+    print("7. Uppercase")
+    print("8. Lowercase")
+    print("9. Replace Word")
+    print("10. Exit")
+    choice = input("Enter choice: ")
+    file = open("text.txt","r")
+    data = file.read()
+    file.close()
 
-print("Data written to file")
+    if choice == "1":
+        print(data)
 
-print("------")
+    elif choice == "2":
+        print("Word count:", len(data.split()))
 
-# Read file
-file = open("text.txt","r")
-data = file.read()
-file.close()
+    elif choice == "3":
+        print("Line count:", len(data.split("\n")))
 
-print("File Content:")
-print(data)
+    elif choice == "4":
+        print("Character count:", len(data))
 
-print("------")
+    elif choice == "5":
+        count = 0
+        for i in data:
+            if i.lower() in "aeiou":
+                count += 1
+        print("Vowels:", count)
 
-# Word count
-words = data.split()
-print("Word count:", len(words))
+    elif choice == "6":
+        word = input("Enter word: ")
+        print("Count:", data.count(word))
 
-print("------")
+    elif choice == "7":
+        print(data.upper())
 
-# Line count
-lines = data.split("\n")
-print("Line count:", len(lines))
+    elif choice == "8":
+        print(data.lower())
 
-print("------")
+    elif choice == "9":
+        old = input("Old word: ")
+        new = input("New word: ")
+        print(data.replace(old, new))
 
-# Character count
-print("Character count:", len(data))
+    elif choice == "10":
+        print("Exit")
+        break
 
-#added more features
-print("more features")
-
-# Vowel count
-count = 0
-for i in data:
-    if i.lower() in "aeiou":
-        count += 1
-print("Vowel count:", count)
-
-print("------")
-
-# Count specific word
-word = input("Enter word to search: ")
-print("Count:", data.count(word))
-
-print("------")
-
-# Uppercase and Lowercase
-print("Uppercase:")
-print(data.upper())
-
-print("Lowercase:")
-print(data.lower())
-
-print("------")
-
-# Replace word
-old = input("Enter word to replace: ")
-new = input("Enter new word: ")
-new_data = data.replace(old, new)
-
-print("After replace:")
-print(new_data)
+    else:
+        print("Invalid choice")
