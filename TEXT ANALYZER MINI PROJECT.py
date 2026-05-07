241c612995f8f303b23477e6f533ca14e9c97463
MAGESGWARI M <mageshwarim9003998405@gmail.com>
2026-05-07 15:37:28 +0530

    Update TEXT ANALYZER MINI PROJECT.py

--- TEXT ANALYZER MINI PROJECT.py
+++ TEXT ANALYZER MINI PROJECT.py
@@ -1,10 +1,11 @@
-print("TEXT ANALYZER PROJECT")
+print("------ TEXT ANALYZER PROJECT ------")
 # Write data to file
-file = open("text.txt","w")
+file = open("text.txt", "w")
 text = input("Enter text: ")
 file.write(text)
 file.close()
-while True:
+# Function for menu
+def menu():
     print("\n1. Show Content")
     print("2. Word Count")
     print("3. Line Count")
@@ -15,48 +16,50 @@ while True:
     print("8. Lowercase")
     print("9. Replace Word")
     print("10. Exit")
+while True:
+    menu()
     choice = input("Enter choice: ")
-    file = open("text.txt","r")
+    file = open("text.txt", "r")
     data = file.read()
     file.close()
-
-    if choice == "1":
+    # Empty file check
+    if data == "":
+        print("File is empty")
+    elif choice == "1":
+        print("\nFile Content:")
         print(data)
-
     elif choice == "2":
         print("Word count:", len(data.split()))
-
     elif choice == "3":
         print("Line count:", len(data.split("\n")))
-
     elif choice == "4":
         print("Character count:", len(data))
-
+        print("Without spaces:", len(data.replace(" ", "")))
     elif choice == "5":
         count = 0
         for i in data:
             if i.lower() in "aeiou":
                 count += 1
-        print("Vowels:", count)
-
+        print("Vowel count:", count)
     elif choice == "6":
         word = input("Enter word: ")
-        print("Count:", data.count(word))
-
+        print("Count:", data.lower().count(word.lower()))
     elif choice == "7":
         print(data.upper())
-
     elif choice == "8":
         print(data.lower())
-
     elif choice == "9":
         old = input("Old word: ")
         new = input("New word: ")
-        print(data.replace(old, new))
-
+        new_data = data.replace(old, new)
+        file = open("text.txt", "w")
+        file.write(new_data)
+        file.close()
+        print("Word replaced successfully")
     elif choice == "10":
         print("Exit")
         break
-
     else:
         print("Invalid choice")
+
+
